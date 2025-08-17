6fb7498b07206ed8f62745c2d40b799e516e2f9e
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-08-17 09:21:44 +0100

    update my cors middleware for allowed_origins

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000/", "https://fileconverter-be.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
